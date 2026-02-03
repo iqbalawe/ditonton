9bc329add2bd0a28af61a749c7ae14c5ee54f417
iqbalwahyudin <iqbal.ajie.wahyudin@bni.co.id>
2026-02-03 22:45:33 +0700

    fix: add builder into search tv series

--- packages/tv_series/lib/presentation.dart/pages/search_tv_series_page.dart
+++ packages/tv_series/lib/presentation.dart/pages/search_tv_series_page.dart
@@ -11,52 +11,59 @@ class SearchTVSeriesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => GetIt.instance<SearchTVSeriesCubit>(),
-      child: Scaffold(
-        appBar: AppBar(title: const Text('Search')),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextField(
-                onSubmitted: (query) {
-                  context.read<SearchTVSeriesCubit>().onQueryChanged(query);
-                },
-                decoration: const InputDecoration(
-                  hintText: 'Search title',
-                  prefixIcon: Icon(Icons.search),
-                  border: OutlineInputBorder(),
-                ),
-                textInputAction: TextInputAction.search,
-              ),
-              const SizedBox(height: 16),
-              Text('Search Result', style: heading6),
-              Expanded(
-                child: BlocBuilder<SearchTVSeriesCubit, SearchTVSeriesState>(
-                  builder: (context, state) {
-                    return state.when(
-                      initial: () => const SizedBox(),
-                      loading: () =>
-                          const Center(child: CircularProgressIndicator()),
-                      error: (msg) => Center(child: Text(msg)),
-                      empty: () =>
-                          const Center(child: Text('TV Series not found')),
-                      loaded: (tvSeries) {
-                        return ListView.builder(
-                          itemBuilder: (context, index) {
-                            final tv = tvSeries[index];
-                            return TvSeriesCard(tv);
+      child: Builder(
+        builder: (context) {
+          return Scaffold(
+            appBar: AppBar(title: const Text('Search')),
+            body: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextField(
+                    onSubmitted: (query) {
+                      context.read<SearchTVSeriesCubit>().onQueryChanged(query);
+                    },
+                    decoration: const InputDecoration(
+                      hintText: 'Search title',
+                      prefixIcon: Icon(Icons.search),
+                      border: OutlineInputBorder(),
+                    ),
+                    textInputAction: TextInputAction.search,
+                  ),
+                  const SizedBox(height: 16),
+                  Text('Search Result', style: heading6),
+                  Expanded(
+                    child:
+                        BlocBuilder<SearchTVSeriesCubit, SearchTVSeriesState>(
+                          builder: (context, state) {
+                            return state.when(
+                              initial: () => const SizedBox(),
+                              loading: () => const Center(
+                                child: CircularProgressIndicator(),
+                              ),
+                              error: (msg) => Center(child: Text(msg)),
+                              empty: () => const Center(
+                                child: Text('TV Series not found'),
+                              ),
+                              loaded: (tvSeries) {
+                                return ListView.builder(
+                                  itemBuilder: (context, index) {
+                                    final tv = tvSeries[index];
+                                    return TvSeriesCard(tv);
+                                  },
+                                  itemCount: tvSeries.length,
+                                );
+                              },
+                            );
                           },
-                          itemCount: tvSeries.length,
-                        );
-                      },
-                    );
-                  },
-                ),
+                        ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
     );
   }
